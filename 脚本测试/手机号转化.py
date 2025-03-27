f06627860b9d4b7c07618f839800e2670d53ec2b
renzhuangzhuang <15525748876@163.com>
2025-03-27 12:33:27 +0800

    删除

--- 脚本测试/手机号转化.py
+++
@@ -1,7 +0,0 @@
-from passlib.context import CryptContext
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-
-print(get_password_hash("18201120708"))
